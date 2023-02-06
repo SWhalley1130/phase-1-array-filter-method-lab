5c915d26633eeec14c72df830240e87cf1afa6ac
Sarah Whalley <s.whalley1130@gmail.com>
2023-02-05 17:56:11 -0700

    Completes lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution here
+const findMatching=(array, name)=> array.filter(e=>e.toLowerCase()===name.toLowerCase());
+
+const fuzzyMatch=(array, name)=>array.filter(e=>e.charAt(0)===name.charAt(0));
+
+const matchName=(arrayOfObjs,nameInput)=>arrayOfObjs.filter(e=>e.name===nameInput);
